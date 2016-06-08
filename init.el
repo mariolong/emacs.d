6b3d5d5004753dd0f2db4d809b5f6930020b3c11
mario <mariolong5782@gmail.com>
2016-06-08 10:26:32 +0800

    cleanup .gitignore
    Please enter the commit message for your changes. Lines starting

--- init.el
+++ init.el
@@ -1,12 +1,15 @@
-(modify-all-frames-parameters '((fullscreen . maximized)))
-(setq inhibit-startup-message t
-      inhibit-startup-echo-area-message t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(modify-all-frames-parameters '((fullscreen . maximized)))
 
-(eval-when-compile (package-initialize))
-(eval-when-compile (require 'cl))
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+
+(package-initialize)
 
 (setq user-emacs-directory (file-name-directory (or load-file-name (buffer-file-name))))
 (setq org-confirm-babel-evaluate nil)
